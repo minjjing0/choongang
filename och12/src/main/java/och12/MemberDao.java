b00efff85c55758478be7bae575e1d0bb6158d24
ROHMINKYUNG <142300528+minjjing0@users.noreply.github.com>
2024-01-24 22:34:08 +0900

    2024년 1월 24일 수업

--- och12/src/main/java/och12/MemberDao.java
+++ och12/src/main/java/och12/MemberDao.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -42,7 +44,6 @@ public class MemberDao {
 
 	// PreparedStatement
 	public int check(String id, String passwd) throws SQLException {
-	
 		int result  = 0;  				
 		Connection conn = null;
 		String sql  = "select passwd from member2 where id=?"; 
@@ -69,6 +70,29 @@ public class MemberDao {
 		return result;
 	}
 
+	public int confirm(String id) throws SQLException {
+		int result  = 0;  				
+        Connection conn = null;
+		String sql  = "select id from member2 where id=?"; 
+		PreparedStatement pstmt = null; 
+		ResultSet rs = null;
+		try { 
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if (rs.next()) result = 1;
+			else result = 0;
+		} catch(Exception e) { 
+			System.out.println(e.getMessage());
+		} finally {
+			if (rs != null) rs.close();
+			if (pstmt != null) pstmt.close();
+			if (conn != null) conn.close();
+		}
+		return result;
+	}
+	
 	public int insert(Member2 member) throws SQLException {
 		int result = 0;  				
         Connection conn = null;
@@ -84,6 +108,89 @@ public class MemberDao {
 			pstmt.setString(5, member.getTel());
 			result = pstmt.executeUpdate();
 		} catch(Exception e) { System.out.println(e.getMessage());
+		} finally {
+			if (pstmt != null) pstmt.close();
+			if (conn != null) conn.close();
+		}
+		return result;
+	}
+	
+	public List<Member2> list() throws SQLException {
+		List<Member2> list = new ArrayList<Member2>();
+		Connection conn = null;
+		String sql = "select * from member2"; 
+		PreparedStatement pstmt = null; 
+		ResultSet rs = null;
+		try { 
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				do {
+					Member2 member = new Member2();
+					member.setId(rs.getString(1));
+					member.setPasswd(rs.getString(2));
+					member.setName(rs.getString(3));
+					member.setAddress(rs.getString(4));
+					member.setTel(rs.getString(5));
+					member.setReg_date(rs.getDate(6));
+					list.add(member);
+				} while(rs.next());
+			} 
+		} catch(Exception e) { 
+			System.out.println(e.getMessage());
+		} finally {
+			if (rs != null) rs.close();
+			if (pstmt != null) pstmt.close();
+			if (conn != null) conn.close();
+		}
+		return list;
+	}
+	
+	public Member2 select(String id) throws SQLException {
+		Member2 member = new Member2();	
+		Connection conn = null;
+		String sql  = "select * from member2 where id=?"; 
+		PreparedStatement pstmt = null; 
+		ResultSet rs = null;
+		try { 
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				member.setId(rs.getString(1));
+				member.setPasswd(rs.getString(2));
+				member.setName(rs.getString(3));
+				member.setAddress(rs.getString(4));
+				member.setTel(rs.getString(5));
+				member.setReg_date(rs.getDate(6));
+			} 
+		} catch(Exception e) { 
+			System.out.println(e.getMessage());
+		} finally {
+			if (rs != null) rs.close();
+			if (pstmt != null) pstmt.close();
+			if (conn != null) conn.close();
+		}
+		return member;
+	}
+
+	public int delete(String id, String passwd) throws SQLException {
+		int result  = 0;  				
+		Connection conn = null;
+		
+		result = check(id, passwd);
+		if (result != 1)  return result;
+		String sql  = "delete from member2 where id=?"; 
+		PreparedStatement pstmt = null; 				
+		try { 
+			conn  = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			result = pstmt.executeUpdate();
+		} catch(Exception e) { 
+			System.out.println(e.getMessage());
 		} finally {
 			if (pstmt != null) pstmt.close();
 			if (conn != null) conn.close();
